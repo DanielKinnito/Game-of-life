fd1fabf868c5549db8f100f354b3fd0080fbfa7e
DanielKinnito <mokonnendaniel@gmail.com>
2024-03-29 23:30:15 +0300

    Finished Phase 1.  Added Tests

--- game_of_life.py
+++ game_of_life.py
@@ -1,3 +1,4 @@
+import time
 import random
 
 def dead_state(width: int, height: int):
@@ -31,7 +32,41 @@ def render(board_state):
     
     return 
 
-render(random_state(5,5))
+def next_board_state(initial_board_state):
+    width = len(initial_board_state[0])
+    height = len(initial_board_state)
+    
+    return_board = dead_state(width, height)
+    
+    for i in range(len(initial_board_state)):
+        for j in range(len(initial_board_state[0])):
+            count = 0
+            
+            for x in range(max(0, i - 1), min(height, i + 2)):
+                for y in range(max(0, j - 1), min(width, j + 2)):
+                    count += initial_board_state[x][y]
+            
+            count -= initial_board_state[i][j]  # Subtract the current cell itself
+            
+            if initial_board_state[i][j] == 1:
+                if count <= 1 or count >= 4:
+                    return_board[i][j] = 0
+                else:
+                    return_board[i][j] = 1
+            else:
+                if count == 3:
+                    return_board[i][j] = 1
+    
+    return return_board
 
+def run_forever(init_state):
+    next_state = init_state
+    while True:
+        render(next_state)
+        next_state = next_board_state(next_state)
+        time.sleep(0.6)
 
-print(random_state(5,5))
+if __name__ == "__main__":
+    init_state = random_state(10, 10)
+    # init_state = load_board_state('./toad.txt')
+    run_forever(init_state)
